3d3de33f494bba912a376e70475df26c37c0824c
akomarla <124313756+akomarla@users.noreply.github.com>
2023-10-13 03:44:57 -0700

    Fixing inconsistency in tag num and version num
    Precaution. No issue so far.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (this_directory / "README.md").read_text()
 # Setting up
 setup(
         name="ExpSmoothing", 
-        version='0.1.0',
+        version='0.1.2',
         author="Aparna Komarla",
         author_email="<aparna.komarla@email.com>",
         description='Exponential smoothing forecast model',
